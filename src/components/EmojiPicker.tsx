f5f81ee0df11bb08ebfc9f9cc8f72b60ecd8e707
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-10-27 14:09:48 +0100

    Ran Prettier

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -2,25 +2,55 @@ import { useState } from "react";
 
 export default function EmojiPicker(): JSX.Element {
   const [emojiValueFromCurrentRender, queueRerenderWithNewEmojiValue] =
-    useState<string>('');
+    useState<string>("");
   const [recentValueFromCurrentRender, queueRerenderWithNewRecentValue] =
-    useState<string>('');
+    useState<string>("");
 
   return (
     <>
       <h1>Emoji picker</h1>
       <p>Previously selected emoji: {recentValueFromCurrentRender}</p>
       <p>Selected emoji: {emojiValueFromCurrentRender}</p>
-      <button onClick={() => {queueRerenderWithNewEmojiValue("😀");
-      queueRerenderWithNewRecentValue(emojiValueFromCurrentRender);}}>😀</button>
-      <button onClick={() => {queueRerenderWithNewEmojiValue("😠");
-      queueRerenderWithNewRecentValue(emojiValueFromCurrentRender);}}>😠</button>
-      <button onClick={() => {queueRerenderWithNewEmojiValue("🍉");
-      queueRerenderWithNewRecentValue(emojiValueFromCurrentRender);}}>🍉</button>
-      <button onClick={() => {queueRerenderWithNewEmojiValue("⚽");
-      queueRerenderWithNewRecentValue(emojiValueFromCurrentRender);}}>⚽</button>
-      <button onClick={() => {queueRerenderWithNewEmojiValue("🗼");
-      queueRerenderWithNewRecentValue(emojiValueFromCurrentRender);}}>🗼</button>
+      <button
+        onClick={() => {
+          queueRerenderWithNewEmojiValue("😀");
+          queueRerenderWithNewRecentValue(emojiValueFromCurrentRender);
+        }}
+      >
+        😀
+      </button>
+      <button
+        onClick={() => {
+          queueRerenderWithNewEmojiValue("😠");
+          queueRerenderWithNewRecentValue(emojiValueFromCurrentRender);
+        }}
+      >
+        😠
+      </button>
+      <button
+        onClick={() => {
+          queueRerenderWithNewEmojiValue("🍉");
+          queueRerenderWithNewRecentValue(emojiValueFromCurrentRender);
+        }}
+      >
+        🍉
+      </button>
+      <button
+        onClick={() => {
+          queueRerenderWithNewEmojiValue("⚽");
+          queueRerenderWithNewRecentValue(emojiValueFromCurrentRender);
+        }}
+      >
+        ⚽
+      </button>
+      <button
+        onClick={() => {
+          queueRerenderWithNewEmojiValue("🗼");
+          queueRerenderWithNewRecentValue(emojiValueFromCurrentRender);
+        }}
+      >
+        🗼
+      </button>
     </>
   );
 }
